6df9a6a47bc5f3e6fde34e29d3fa73226139a2d1
Daniel Lopes <dlopes4786@gmail.com>
2025-05-27 20:37:55 -0300

    Criando Contrutor e Atributos

--- sistema/MovimentacaoEstoque.java
+++ sistema/MovimentacaoEstoque.java
@@ -1,3 +1,18 @@
 public abstract class MovimentacaoEstoque {
+    
+    int quantidade;
+    String data;
+    String tipo;
+
+
+
+    
+    public MovimentacaoEstoque (int quantidade, String data,String tipo){
+        this.quantidade = quantidade;
+        this.data = data;
+        this.tipo = tipo;
+    }
+
+
 
 }
