f376dd448e3cd9bb7ade82e835c91b4aa3dc3916
Daniel Lopes <dlopes4786@gmail.com>
2025-05-27 21:41:42 -0300

    Estruturando classe Entrada

--- sistema/Entrada.java
+++ sistema/Entrada.java
@@ -1,3 +1,18 @@
-public class Entrada {
+public class Entrada extends MovimentacaoEstoque implements Processavel{
+
+    public Entrada (int quantidade, String data){
+        super(quantidade,data);
+        this.tipo = "Entrada";
+    }
+
+    @Override
+    public boolean processarMovimentacao() {
+    }
+
+    @Override
+    public boolean validarMovimentacao() {
+    }
+
+    
 
 }
